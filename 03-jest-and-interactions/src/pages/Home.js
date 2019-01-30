b0b7e477ef34bc22ad346c199bfa6b5c08838e2c
hyosunko <hyosunko@gmail.com>
2019-01-30 14:56:42 -0800

    jest-and-interactions

--- 03-jest-and-interactions/src/pages/Home.js
+++ 03-jest-and-interactions/src/pages/Home.js
@@ -11,13 +11,15 @@ class Home extends Component {
   render() {
     const{ flashMessage } = this.state
     const message = 'Congratulations! You are the 1 millionth clicker of this button!'
+    const message2 = 'We knew you would'
     return(
       <div>
+
         <div id="flash_message">{flashMessage}</div>
         <button
-          id="click_me"
-          onClick={ () => this.setState({ flashMessage: message}) }
-        >Click Me!</button>
+          id="dont_click_me"
+          onClick={ () => this.setState({ flashMessage: message2}) }
+        >Don't Click Me!</button>
       </div>
     )
   }
